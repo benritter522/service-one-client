b003a04c8eafdc86eea65a1507b3d566264d3db3
David Tersoff <daveboy21@gmail.com>
2021-03-24 18:17:04 -0400

    Making further improvements to ProfileCard

--- src/Components/ProfileCard/index.js
+++ src/Components/ProfileCard/index.js
@@ -9,12 +9,16 @@ class ProfileCard extends Component {
 
     return (
       <Base class={'card'}>
-      <div class='cardHeader'>{this.props.business_name}</div>
-      <div class='cardBody'>
-        <div>{this.props.street_address},</div>
-        <div>{this.props.city}, {this.props.state} {this.props.zip_code_first5}</div>
+      <img src={`${process.env.PUBLIC_URL}/images/placeholder.jpg`} alt='placeholder'/>
+      <div class='text'>
+        <div class='cardHeader'>{this.props.business_name}</div>
+        <div class='cardBody'>
+          <div>{this.props.street_address},</div>
+          <div>{this.props.city}, {this.props.state} {this.props.zip_code_first5}</div>
+        </div>
       </div>
       </Base>
+      
     );
   }
 
