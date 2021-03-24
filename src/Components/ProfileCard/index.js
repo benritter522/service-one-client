6143a15b0327d7b23a141232e19a3ef3f6ac74fc
David Tersoff <daveboy21@gmail.com>
2021-03-24 15:44:53 -0400

    Making updates to ProfileCard

--- src/Components/ProfileCard/index.js
+++ src/Components/ProfileCard/index.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import { Card as Base } from '@material-ui/core';
 import PropTypes from 'prop-types';
+import './index.css'
 
 class ProfileCard extends Component {
 
   render() {
 
     return (
-      <>
-      <h1>{this.props.business_name}</h1>
-      <div>{this.props.street_address},</div>
-      <div>{this.props.city}, {this.props.state} {this.props.zip_code_first5}</div>
-      < />
+      <Base class={'card'}>
+      <div class='cardHeader'>{this.props.business_name}</div>
+      <div class='cardBody'>
+        <div>{this.props.street_address},</div>
+        <div>{this.props.city}, {this.props.state} {this.props.zip_code_first5}</div>
+      </div>
+      </Base>
     );
   }
 
